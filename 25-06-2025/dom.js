a16f0f7b4cc1a86438cea82ec995beaf2547ce85
rohit <rohitvarma951@gmail.com>
2025-06-25 15:33:35 +0530

    Push files at the end of Day 25-06-2025

---
+++ 25-06-2025/dom.js
@@ -0,0 +1,19 @@
+document.title = "DOM in JavaScript";
+
+document.getElementById("header").innerHTML =
+    "Hello, Java Script";
+document.getElementById("para").style.color = "blue";
+document.getElementById("para").style.fontSize = "30px";
+document.getElementsByClassName("joke")[0].innerHTML = "Hello";
+document.getElementsByClassName("joke")[1].innerHTML = "This is Java Script";
+document.getElementsByClassName("joke")[0].style.color = "red";
+document.getElementsByClassName("joke")[1].style.color = "black";
+document.getElementsByTagName("h1")[0].style.color = "green";
+document.body.style.backgroundColor = "black";
+
+
+
+
+
+
+
